89fc4e35ad02b799844bb37e5e99822590c7811a
З - 9 < - 9@DESKTOP-7SA1BQD>
2022-09-09 20:42:20 +0500

    Добавьте файлы проекта.

---
+++ evelopments(sobitia)29.09.2022/Acc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evelopments_sobitia_29._09._2022 {
+
+    internal class Acc {
+        public delegate void AccHandler(string sender);
+
+        public event AccHandler? Notify;
+
+        public Acc(int sum) {
+            Sum = sum;
+            Notify?.Invoke($"На счет поступило: {sum}");
+        }
+
+        public int Sum { get; set; }
+
+        public void Put(int sum) 
+        { 
+            Sum += sum;
+            Console.WriteLine($"На счет поступило: " +
+                $"{sum}");
+        }
+
+        public void Take(int sum) 
+        {
+            if (Sum >= sum) {
+                Sum -= sum;
+                Notify?.Invoke($"Со счета снято: {sum}");
+            }
+            else 
+            {
+                Notify?.Invoke($"Недостаточно средств.");
+            }
+        }
+    }
+}
---
+++ evelopments(sobitia)29.09.2022/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using evelopments_sobitia_29._09._2022;
+
+void PrintMes(string mes) 
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(mes);
+}
+
+Acc acc1 = new Acc(1000);
+
+acc1.Notify += PrintMes;
+
+acc1.Put(20);
+
+acc1.Take(70);
+
+//Console.WriteLine($"Сумма на счете: {acc1.Sum}");
+
+acc1.Take(180);
+
+//Console.WriteLine($"Сумма на счете: {acc1.Sum}");
